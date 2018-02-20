0dfbeb658963bea0e55e171a7d2455c514943773
周子鉴 <blqw@live.com>
2018-02-20 17:31:04 +0800

    IntPtr & UIntPtr 转换器

---
+++ src/Convert4/Convertors/Primitive/IntPtrConvertor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blqw.Convertors
+{
+    public class IntPtrConvertor : BaseConvertor<IntPtr>
+                                 , IFrom<int, IntPtr>
+                                 , IFrom<long, IntPtr>
+                                 , IFrom<object, IntPtr>
+    {
+        public IntPtr From(ConvertContext context, long input) => new IntPtr(input);
+        public IntPtr From(ConvertContext context, int input) => new IntPtr(input);
+        public IntPtr From(ConvertContext context, object input)
+        {
+            var result = context.ChangeType<long>(input);
+            if (!result.Success)
+            {
+                context.Exception = context.InvalidCastException(input, TypeFriendlyName) + result.Error;
+                return default;
+            }
+            return new IntPtr(result.OutputValue);
+        }
+    }
+}
